d8db5ffe1f749496dbf60e7ec868177b22fd11e8
Torgeir Skjøtskift <torgeir.skjotskift@gmail.com>
2023-04-19 12:24:39 +0200

    initial commit

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import express from "express"
+
+const app = express()
+
+app.get('/', (req, res) => {
+  res.send('Hello from my-module')
+})
+
+app.listen(3000)
+console.log('Express started on port 3000');
+
