80847d8d1f1dbd183a9c735efca55594221696f0
torgeirsk <30318122+torgeirsk@users.noreply.github.com>
2023-04-28 13:00:54 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import express from "express"
-
+const { MY_NAME } = process.env
 const app = express()
 
 app.get('/', (req, res) => {
-  res.send('Hello from my-module')
+  res.send(`Hello, ${MY_NAME}!`)
 })
 
 app.listen(3000)
