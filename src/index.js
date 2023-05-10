f04bfc9a7fb8db20f5221d3ef5356b05e6d6fc45
miaskjo <30318122+miaskjo@users.noreply.github.com>
2023-05-10 13:36:14 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import express from "express"
-const { myName, secret } = process.env
+const { myName, password } = process.env
 const app = express()
 
 app.get('/', (req, res) => {
-  res.send(`Hello, ${myName}! The password is ${secret}`)
+  res.send(`Hello, ${myName}! The password is ${password}`)
 })
 
 app.listen(3000)
